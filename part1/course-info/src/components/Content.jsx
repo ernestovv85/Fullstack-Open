ef17e748904bd383dbbf8678f15605928e412950
ernestovv85 <ernestovv85@gmail.com>
2024-05-21 16:15:49 -0600

    add components with props

---
+++ part1/course-info/src/components/Content.jsx
@@ -0,0 +1,20 @@
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1}
+        {props.exercises1}
+      </p>
+      <p>
+        {props.part2}
+        {props.exercises2}
+      </p>
+      <p>
+        {props.part2}
+        {props.exercises3}
+      </p>
+    </div>
+  );
+}
+
+export default Content
